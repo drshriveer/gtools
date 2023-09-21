74a776f7d9d7f51dbbfcc4f8da6f8b6c573ec3c5
Gavin Shriver <g@stately.cloud>
2023-09-20 23:46:49 -0700

    improve sacks!

--- gerror/error_test.go
+++ gerror/error_test.go
@@ -3,6 +3,8 @@ package gerror_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/drshriveer/gtools/gerror"
@@ -20,7 +22,7 @@ func (a AType) ReturnsError() error {
 	return ErrMyError1.Stack()
 }
 
-func (a AType) InlineError() error {
+func (a AType) InlineError2XL() error {
 	return func() error {
 		return func() error {
 			return ErrMyError1.Stack()
@@ -28,8 +30,18 @@ func (a AType) InlineError() error {
 	}()
 }
 
+func (a AType) InlineError3XL() error {
+	return func() error {
+		return func() error {
+			return func() error {
+				return ErrMyError1.Stack()
+			}()
+		}()
+	}()
+}
+
 func TestGError_WithStack(t *testing.T) {
-	err := ErrMyError1.Stack().(gerror.Error)
+	err := ErrMyError1.Stack()
 	assert.NotSame(t, ErrMyError1, &err)
 	assert.Equal(t, "gerror_test:TestGError_WithStack", err.ErrSource())
 	assert.NotEmpty(t, err.ErrStack())
@@ -38,8 +50,8 @@ func TestGError_WithStack(t *testing.T) {
 	assert.Empty(t, ErrMyError1.(gerror.Error).ErrStack())
 	assert.Empty(t, ErrMyError1.(gerror.Error).ErrStack())
 
-	strukt := &AType{}
-	err = strukt.ReturnsError().(gerror.Error)
+	aType := &AType{}
+	err = aType.ReturnsError().(gerror.Error)
 	assert.NotSame(t, ErrMyError1, &err)
 	assert.Equal(t, "gerror_test:AType:ReturnsError", err.ErrSource())
 	assert.NotEmpty(t, err.ErrStack())
@@ -47,17 +59,134 @@ func TestGError_WithStack(t *testing.T) {
 	// ensure unchanged:
 	assert.Empty(t, ErrMyError1.(gerror.Error).ErrSource())
 	assert.Empty(t, ErrMyError1.(gerror.Error).ErrStack())
+}
 
-	err = strukt.InlineError().(gerror.Error)
-	assert.Equal(t, "gerror_test:AType:ReturnsError", err.ErrSource())
-	for _, el := range err.ErrStack() {
-		println(el.Metric())
+//nolint:errname // for testing
+var pkgVarInlineFunc1 = func() error {
+	return ErrMyError1.Stack()
+}()
+
+func pkgFuncInlineErr1() error {
+	return func() error {
+		return ErrMyError1.Stack()
+	}()
+}
+
+func pkgFuncInlineErr2() error {
+	return func() error {
+		return func() error {
+			return ErrMyError1.Stack()
+		}()
+	}()
+}
+
+func inlineAndOtherDeepLayer() error {
+	aType := &AType{}
+	return func() error {
+		return aType.InlineError2XL()
+	}()
+}
+
+func genericFuncResponse[T any](_ T) error {
+	return ErrMyError1.Stack()
+}
+
+func TestStackSource(t *testing.T) {
+	aType := &AType{}
+	tests := []struct {
+		description string
+		err         error
+
+		expected string
+	}{
+		{
+			// Looks like: gerror_test:TestStackSource
+			description: "local (instruct)",
+			err:         ErrMyError1.Stack(),
+			expected:    "gerror_test:TestStackSource",
+		},
+		{
+			// Looks like: gerror_test:TestStackSource:func1
+			description: "local (in instruct> anonymous",
+			err: func() error {
+				return ErrMyError1.Stack()
+			}(),
+			expected: "gerror_test:TestStackSource",
+		},
+		{
+			// Looks Like: gerror_test:AType:ReturnsError
+			description: "struct method returns error",
+			err:         aType.ReturnsError(),
+			expected:    "gerror_test:AType:ReturnsError",
+		},
+		{
+			// Looks Like: github.com/drshriveer/gtools/gerror_test.AType.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like: <PKG>.AType.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like: <PKG>.<TYPE>.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like:
+			description: "struct method > 2 layers of anonymous functions",
+			err:         aType.InlineError2XL(),
+			// is: gerror_test:AType:InlineError2XL:AType:InlineError2XL
+			expected: "gerror_test:AType:InlineError2XL",
+		},
+		{
+			// Looks Like: github.com/drshriveer/gtools/gerror_test.TestStackSource.AType.InlineError3XL.func3.1.1
+			// Looks Like: <PKG>.TestStackSource.AType.InlineError3XL.func3.1.1
+			// Looks Like: <PKG>.<nearest-function>.AType.InlineError3XL.func3.1.1
+			// Looks Like: <PKG>.<nearest-function>.<type>.<type-function>.func3.1.1
+			description: "struct method > 3 layers of anonymous functions",
+			err:         aType.InlineError3XL(),
+			expected:    "gerror_test:TestStackSource:AType:InlineError3XL",
+		},
+		{
+			// Looks Like: github.com/drshriveer/gtools/gerror_test.glob..func1
+			// Looks Like: <PKG>.glob..func1
+			description: "pkg var > anonymous",
+			err:         pkgVarInlineFunc1,
+			expected:    "gerror_test:glob",
+		},
+		{
+			// Looks Like: github.com/drshriveer/gtools/gerror_test.TestStackSource.pkgFuncInlineErr1.func4
+			// Looks Like: <PKG>.TestStackSource.pkgFuncInlineErr1.func4
+			// Looks Like: <PKG>.<nearest-function>.pkgFuncInlineErr1.func4
+			// Looks Like: <PKG>.<nearest-function>.<named-func>.func4
+			description: "pkg func> anonymous 1",
+			err:         pkgFuncInlineErr1(),
+			expected:    "gerror_test:TestStackSource:pkgFuncInlineErr1",
+		},
+		{
+			// Looks Like: <PKG>.pkgFuncInlineErr2.pkgFuncInlineErr2.func1.func2
+			description: "pkg func> anonymous 2",
+			err:         pkgFuncInlineErr2(),
+			expected:    "gerror_test:pkgFuncInlineErr2",
+		},
+		{
+			// Looks Like: github.com/drshriveer/gtools/gerror_test.AType.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like: <PKG>.AType.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like: <PKG>.<type>.InlineError2XL.AType.InlineError2XL.func1.func2
+			// Looks Like: <PKG>.<type>.<type-func>.AType.InlineError2XL.func1.func2
+			description: "oddities at large",
+			err:         inlineAndOtherDeepLayer(),
+			expected:    "gerror_test:AType:InlineError2XL",
+		},
+		{
+			description: "a generic what",
+			err:         genericFuncResponse[string]("bhahahahhaha"),
+			expected:    "gerror_test:genericFuncResponse",
+		},
 	}
 
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			gerr, ok := test.err.(gerror.Error)
+			require.True(t, ok)
+			assert.Equal(t, test.expected, gerr.ErrSource())
+		})
+	}
 }
 
 func TestGError_ExtMsgf(t *testing.T) {
-	err := ErrMyError1.ExtMsgf("T-Shirts $%d", 5).(gerror.Error)
+	err := ErrMyError1.ExtMsgf("T-Shirts $%d", 5)
 	assert.NotSame(t, ErrMyError1, &err)
 	assert.Equal(t, "gerror_test:TestGError_ExtMsgf", err.ErrSource())
 	assert.NotEmpty(t, err.ErrStack())
--- gerror/stack.go
+++ gerror/stack.go
@@ -15,8 +15,9 @@ const (
 	NoStack StackType = 0
 	// SourceStack retrieves the minimum sack possible to populate "source".
 	// Note; this is a noop for errors with a defined source.
-	// CAUTION: this is dubious... We're adding multiple stack elements
-	SourceStack StackType = 1
+	// CAUTION: this is dubious... We're adding multiple stack elements to try and find
+	// a more valid stack.... who knows if it will work.
+	SourceStack StackType = 4
 	// ShortStack gets a max stack of 16 elements.
 	ShortStack StackType = 16
 	// DefaultStack gets a max stack of 32 elements.
@@ -69,7 +70,7 @@ func getCurrentPackage() (string, bool) {
 
 // StackElem represents a single line in a Stack trace.
 type StackElem struct {
-	// Name is the fully qualified package function path.
+	// Name is the fully qualified package function path (?).
 	// e.g. github.com/drshriveer/gtools/gerror.TestGError_WithStack
 	Name string
 
@@ -80,34 +81,70 @@ type StackElem struct {
 	LineNumber int
 }
 
-// SourceInfo returns info about where this error was propagated including packageName, typeName, and functionName.
-func (e StackElem) SourceInfo() (packageName string, typeName string, funcName string) {
+// SourceInfo returns info about where this error was propagated including packageName,
+// and other identifying information.
+// The ambiguity of the other identifying information is unfortunate; but that's what we're
+// starting with. I'd love info here...
+func (e StackElem) SourceInfo() (packageName string, parts []string) {
 	splitName := strings.Split(e.Name, "/")
 	last := splitName[len(splitName)-1]
 	// Next step: handle generics which show up as funcName[....]
 	// I'd love to do:
 	//    last = strings.Replace(last, "[...]", "[T]", 1)
 	// but this probably isn't metric safe.
-	// I also assume that [...] handles N types so [T] wouldn't quite work.
+	// I also assume that [...] handles N types so maybe [T] wouldn't quite work.
+	// FIXME: very possible the [...] only comes out in specific situaions.
 	last = strings.TrimSuffix(last, "[...]")
+
+	// "last" has a number of variations, I don't yet understand the rules.
+	// (See tests for examples / more info.)
+	// But generally, the first 3 nodes seem to be the most useful.
+	// the first of which is _always_ a package.
 	vals := strings.Split(last, ".")
 
-	if len(vals) == 2 {
-		packageName, funcName = vals[0], vals[1]
-	} else if len(vals) == 3 {
-		packageName, typeName, funcName = vals[0], vals[1], vals[2]
-		typeName = strings.TrimPrefix(typeName, "(")
-		typeName = strings.TrimSuffix(typeName, ")")
+	if len(vals) >= 1 {
+		packageName = vals[0]
+		vals = vals[1:]
 	}
 
-	return packageName, typeName, funcName
+	// cut it down-- anything after funcX is not very useful imo
+	for i, val := range vals {
+		// conditions for truncaing the set.
+		if strings.HasPrefix(val, "func") || val == "" {
+			vals = vals[:i]
+			break
+		}
+	}
+
+	return packageName, vals
 }
 
 // Metric returns a metric-safe(?) string of the source info.
 func (e StackElem) Metric() string {
-	pkg, tName, fName := e.SourceInfo()
-	tName = strings.TrimPrefix(tName, "*") // remove pointer indicator
-	return convertToMetricNode(pkg, tName, fName)
+	// delim is the metric node delimiter.
+	// TODO: reconsider this delimiter when support for it is clearer.
+	const delim = ":"
+
+	pkg, theRest := e.SourceInfo()
+
+	// Experimental .. drop repeats.
+outer:
+	for i := 1; i < len(theRest); i++ {
+		curr := theRest[i]
+		for j := 0; j < i; j++ {
+			if curr == theRest[j] {
+				// only use everything before the repeat.
+				theRest = theRest[:i]
+				// theRest = theRest[:i-1]
+				break outer
+			}
+		}
+	}
+
+	// convertToMetricNode takes a list of string elements and transforms
+	// them into a delimited metric-safe string skipping any empty entries.
+	return pkg + delim + strings.Join(theRest, delim)
+
 }
 
 func makeStack(depth StackType, skip StackSkip) Stack {
