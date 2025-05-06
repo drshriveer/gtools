daa1ae902fb1530ebe8630383a37c0a36d30a854
Gavin Shriver <g@stately.cloud>
2025-05-06 12:22:53 -0700

    using proper logger now..

--- gomonorepo/cmd/gomonorepo/main.go
+++ gomonorepo/cmd/gomonorepo/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -50,7 +49,7 @@ func main() {
 	if fErr, ok := err.(*flags.Error); ok && fErr.Type == flags.ErrHelp {
 		parser.WriteHelp(os.Stdout)
 	} else if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
+		opts.Errorf("Fatal: %w", err)
 		os.Exit(1)
 	}
 }
